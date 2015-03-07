f45e48bb8c83382cc726382a6b1c673b94ceb6a1
Sebastian Ortega <sortega@coinffeine.com>
2015-03-07 18:46:52 +0100

    Expected distribution strategy

---
+++ src/main/scala/spock/ExpectedValueStrategy.scala
@@ -0,0 +1,39 @@
+package spock
+
+import scalaz.Memo
+
+class ExpectedValueStrategy(prob: Distro) {
+
+  type ScoreMap = Map[Int, Double]
+  private case class Scope(attempt: Attempt, range: Range)
+
+  def choose(attempt: Attempt, range: Range): Int = {
+    val scores = expectedScores(Scope(attempt, range))
+    scores.maxBy(_._2)._1
+  }
+
+  private val expectedScores: Scope => ScoreMap = Memo.mutableHashMapMemo { scope =>
+    val scores = for {
+      pivot <- scope.range.iterable
+      (leftRange, rightRange) = scope.range.splitBy(pivot)
+    } yield {
+      def notGuessedScore(remainingRange: Range) = {
+        prob.conditional(remainingRange, scope.range) *
+          expectedScore(Scope(scope.attempt + 1, remainingRange))
+      }
+
+      val whenLower = notGuessedScore(leftRange)
+      val whenGuessed = prob.conditional(pivot, scope.range) * Score(scope.attempt)
+      val whenGreater = notGuessedScore(rightRange)
+
+      whenLower + whenGuessed + whenGreater
+    }
+    scope.range.iterable.zip(scores).toMap
+  }
+
+  private val expectedScore: Scope => Double = Memo.mutableHashMapMemo { scope =>
+    if (scope.range.size == 0) 0
+    else if (scope.range.size == 1 || scope.attempt > Attempt.Max) Score(scope.attempt)
+    else expectedScores(scope).values.max
+  }
+}
---
+++ src/test/scala/spock/ExpectedValueStrategyTest.scala
@@ -0,0 +1,32 @@
+package spock
+
+import org.scalatest.{FlatSpec, ShouldMatchers}
+
+class ExpectedValueStrategyTest extends FlatSpec with ShouldMatchers {
+  val uniformStrategy = new ExpectedValueStrategy(Distro.uniform((1 to 100).toSet))
+
+  "The expected value strategy" should "choose the only option" in {
+    uniformStrategy.choose(attempt = 1, range = Range(1)) shouldBe 1
+    uniformStrategy.choose(attempt = 1, range = Range(50)) shouldBe 50
+  }
+
+  it should "choose the most likely out of two options" in {
+    val strategy = new ExpectedValueStrategy(Distro(1 -> 0.4, 2 -> 0.6))
+    strategy.choose(attempt = 1, range = Range(1, 2)) shouldBe 2
+  }
+
+  it should "choose the central elements when having an uniform distribution" in {
+    val startTime = System.currentTimeMillis()
+    uniformStrategy.choose(attempt = 1, range = Range.Initial) should equal (50 +- 35)
+    uniformStrategy.choose(attempt = 2, range = Range(1, 49)) should equal (25 +- 15)
+    uniformStrategy.choose(attempt = 3, range = Range(51, 74)) should equal (62 +- 10)
+    uniformStrategy.choose(attempt = 1, range = Range(1, 5)) should equal (3 +- 1)
+    val millis = System.currentTimeMillis() - startTime
+    millis should be < 2000L
+  }
+
+  it should "focus on the elements with most probability" in {
+    val strategy = new ExpectedValueStrategy(Distro.uniform((90 to 100).toSet))
+    strategy.choose(attempt = 4, range = Range.Initial) should equal (95 +- 5)
+  }
+}
