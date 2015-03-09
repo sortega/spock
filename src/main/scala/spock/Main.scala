8178a57609a2fab98b28d9040bfb6ab7f4fd1802
Sebastian Ortega <sortega@coinffeine.com>
2015-03-09 11:36:51 +0100

    Bayesian picker distribution estimator

--- src/main/scala/spock/Main.scala
+++ src/main/scala/spock/Main.scala
@@ -1,14 +1,15 @@
 package spock
 
 import spock.forgetful.NaiveForgetfulPicker
-import spock.learning.guesser.{LearningGuesser, FrequencyDistroEstimator}
+import spock.learning.guesser._
 
 object Main {
   def main(args: Array[String]): Unit = args match {
     case Array("pick") =>
       LineOrientedIO(new PickerRunner(new NaiveForgetfulPicker))
     case Array("guess") =>
-      LineOrientedIO(new GuesserRunner(new LearningGuesser(FrequencyDistroEstimator.uniformPrior(1))))
+      //LineOrientedIO(new GuesserRunner(new LearningGuesser(FrequencyDistroEstimator.uniformPrior(1))))
+      LineOrientedIO(new GuesserRunner(new LearningGuesser(new BayesianDistroEstimator)))
     case unexpected =>
       println("Unexpected arguments: " + unexpected)
       println("usage: java -jar <this jar> (pick | guess)")
