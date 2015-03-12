822aa33859d8d1fe9c21004cc4fe836792b595c0
Sebastian Ortega <sortega@coinffeine.com>
2015-03-12 23:43:51 +0100

    Fix usage

--- src/main/scala/spock/Main.scala
+++ src/main/scala/spock/Main.scala
@@ -7,7 +7,7 @@ object Main {
     case Array("pick") => LineOrientedIO(new PickerRunner(Strategy.Picker.Bayesian))
     case Array("guess") => LineOrientedIO(new GuesserRunner(Strategy.Guesser.ExpectedValueBayesian))
     case unexpected =>
-      println("Unexpected arguments: " + unexpected)
+      println("Unexpected arguments: " + unexpected.mkString(" "))
       println("usage: java -jar <this jar> (pick | guess)")
   }
 }
