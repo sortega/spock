a8fb21e601a886d5aacf1d745ed355ba18b0a739
Sebastian Ortega <sortega@coinffeine.com>
2015-04-11 19:19:39 +0200

    Benchmark with 10k rounds

--- src/test/scala/spock/arbiter/BenchmarkMain.scala
+++ src/test/scala/spock/arbiter/BenchmarkMain.scala
@@ -3,5 +3,5 @@ package spock.arbiter
 import spock.Strategy
 
 object BenchmarkMain extends App {
-  new Tournament(Strategy.Pickers, Strategy.Guessers).run(100)
+  new Tournament(Strategy.Pickers, Strategy.Guessers).run(10000, debug = false)
 }
